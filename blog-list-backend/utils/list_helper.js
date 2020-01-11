13ce59102ea9740fba1c63bc8c27a1ccc0fd00e8
aritraChatterji <aritra.chatterji@gmail.com>
2020-01-11 04:00:24 +0100

    first commit

---
+++ blog-list-backend/utils/list_helper.js
@@ -0,0 +1,35 @@
+const dummy = blogs => {
+    return 1;
+};
+
+const totalLikes = blogs => {
+    const reducer = (sum, item) => {
+        return sum + item.likes;
+    };
+    return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
+};
+
+const favoriteBlog = blogs => {
+    let favoriteBlog = null;
+    let maximumLikes = 0;
+
+    blogs.forEach(element => {
+        if (element.likes >= maximumLikes) {
+            favoriteBlog = element;
+            maximumLikes = element.likes;
+        }
+    });
+    return favoriteBlog === null
+        ? null
+        : {
+              title: favoriteBlog.title,
+              author: favoriteBlog.author,
+              likes: favoriteBlog.likes
+          };
+};
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog
+};
